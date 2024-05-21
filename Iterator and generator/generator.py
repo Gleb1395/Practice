973e474059193227a5fa9709be53e4d5ffc7ed5b
Глеб Дубинин <gleb1395@gmail.com>
2024-05-21 23:16:35 +0300

    fixed_2

--- Iterator and generator/generator.py
+++ Iterator and generator/generator.py
@@ -4,7 +4,7 @@ import random
 
 
 def my_generator(count_words: int):
-    english_words = words.words()
+    english_words = list(set(words.words()))
     if count_words > 10_000:
         return None
     elif count_words < 0:
@@ -15,5 +15,7 @@ def my_generator(count_words: int):
         if random_number not in cache:
             cache.add(random_number)
             yield english_words[random_number]
-w = my_generator(15)
-print(list(w))
+
+
+some_words = my_generator(10_000)
+print(len(list(some_words)))
